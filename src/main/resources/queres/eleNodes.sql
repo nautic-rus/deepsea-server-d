11844d7a19dce57463e95ec49e792fe453d2c3a5
dulanova <dinara-dulanova@yandex.ru>
2025-06-20 12:22:40 +0300

    fix: back unnessasary files and delete ele filter (pdf)

--- src/main/resources/queres/eleNodes.sql
+++ src/main/resources/queres/eleNodes.sql
@@ -1,3 +1,4 @@
 -- select * from v_node_penetration np, v_node n where np.NODE = n.NODE
 -- select n.node, n.type from v_node_penetration np, v_node n where np.NODE = n.NODE
-select np.node, np.type from v_node_penetration np where np.type = 2
+-- select np.node, np.type from v_node_penetration np where np.type = 2
+select np.node, np.type from v_node_penetration np
--- src/main/scala/domain/deepsea/pdfGenerator.scala
+++ src/main/scala/domain/deepsea/pdfGenerator.scala
@@ -24,146 +24,146 @@ import java.util.UUID
 import scala.collection.immutable.TreeMap
 //
 object pdfGenerator {
-//  def createPdf(data: Seq[CablesPdf], filteredNodes: Seq[CableNodes], cablesRoutesList: Seq[CableRoutesList]): String = {
-//    println("cableData in def")
-//    try {
-////      val file = Files.createTempFile("spec", ".pdf")
-//      //дата
-//      val currentDate = LocalDate.now()
-//      val year = currentDate.format(DateTimeFormatter.ofPattern("yyyy"))
-//      val month = currentDate.format(DateTimeFormatter.ofPattern("MM"))
-//      val day = currentDate.format(DateTimeFormatter.ofPattern("dd"))
-//
-//      // создание директории
-//      val dirPath = Paths.get("/files")
-//      val yearPath = dirPath.resolve(year)
-//      if (!Files.exists(yearPath)) {
-//        Files.createDirectories(yearPath)
-//      }
-//
-//      val monthPath = yearPath.resolve(month)
-//      if (!Files.exists(monthPath)) {
-//        Files.createDirectories(monthPath)
-//      }
-//
-//      val dayPath = monthPath.resolve(day)
-//      if (!Files.exists(dayPath)) {
-//        Files.createDirectories(dayPath)
-//      }
-//
-//      val randomDirName = UUID.randomUUID().toString.take(10)
-//      val randomDirPath = dayPath.resolve(randomDirName)
-//
-//      if (!Files.exists(randomDirPath)) {
-//        Files.createDirectories(randomDirPath)
-//      }
-//
-//      // Создаем файл PDF в соответствующей директории
-//      var fileName = "file.pdf"
-//      var filePath = randomDirPath.resolve(fileName)
-//      val file = Files.createFile(filePath)
-//
-//      println(file)
-//      val gostFont = PdfFontFactory.createFont(FontProgramFactory.createFont("fonts/GOSTtypeA.ttf"), PdfEncodings.IDENTITY_H, EmbeddingStrategy.PREFER_NOT_EMBEDDED)
-//      val gostFontBold = PdfFontFactory.createFont(FontProgramFactory.createFont("fonts/gost_2.304_Bold.ttf"), PdfEncodings.IDENTITY_H, EmbeddingStrategy.PREFER_NOT_EMBEDDED)
-//      val writer = new PdfWriter(file.toString)
-//      val pdf = new PdfDocument(writer)
-//      val document = new Document(pdf,PageSize.A4.rotate())
-//      val totalWidth = PageSize.A4.getHeight
-//
-//      printTitle(document, 1, totalWidth, gostFont, "КАБЕЛЬНЫЙ ЖУРНАЛ МАГИСТРАЛЬНЫХ КАБЕЛЕЙ", "200101-880-004ВК")  //добавляем таблицу с титульника
-//      document.add(new AreaBreak())  //разрыв страницы
-//
-////      создаем основную таблицу
-//      val columnPercentages = Array(15F, 20F, 15F, 7F, 10F, 10F, 10F, 10F, 15F)
-//      val pointColumnWidths = columnPercentages.map(p => (totalWidth * p / 100).toFloat)
-//      val table = new Table(pointColumnWidths).useAllAvailableWidth()
-//      table.setMarginBottom(60F)
-//
-//      //заполняем шапку основной таблицы
-//      table.addHeaderCell(new Cell(2, 1).add(new Paragraph("Индекс кабеля").setFont(gostFont)))
-//      table.addHeaderCell(new Cell(2, 1).add(new Paragraph("Марка кабеля").setFont(gostFont)))
-//      table.addHeaderCell(new Cell(2, 1).add(new Paragraph("Число жил и сечение, мм2").setFont(gostFont)))
-//      table.addHeaderCell(new Cell(2, 1).add(new Paragraph("Проектная длина, м").setFont(gostFont)))
-//
-//      val fromCell = new Cell(1, 2)
-//      fromCell.add(new Paragraph("Откуда идет кабель").setFont(gostFont))
-//      table.addHeaderCell(fromCell)
-//      val toCell = new Cell(1, 2)
-//      toCell.add(new Paragraph("Куда идет кабель").setFont(gostFont))
-//      table.addHeaderCell(toCell)
-//      // Примечание
-//      table.addHeaderCell(new Cell().add(new Paragraph("Примечание").setFont(gostFont)))
-//
-//
-//      val arr = Seq("индекс", "помещ", "помещ", "индекс", "")
-//      arr.foreach(i => {
-//        val call = new Cell()
-//        call.add(new Paragraph(i).setFont(gostFont))
-//        table.addHeaderCell(call)
-//      })
-//
-//
-//      pdf.addEventHandler(PdfDocumentEvent.END_PAGE, new FooterEventListener(pdf, document, totalWidth, gostFont))
-//
-//      //заполняем данными
-//      try {
-//        val groupedData = data.groupBy(_.system)
-//        val sortedGroupedData = TreeMap(groupedData.toSeq.sortBy(_._1): _*)
-//        sortedGroupedData.foreach { case (system, cables) =>
-//          // Проверяем, есть ли хотя бы один кабель с нодами
-//          val cablesWithNodes = cables.filter(cable => getNodes(cable.cable_id, filteredNodes, cablesRoutesList).nonEmpty)
-//          //          println(sort(cablesWithNodes))
-//          val sortedCablesWithNodes = sort(cablesWithNodes)
-//
-//
-//          if (sortedCablesWithNodes.nonEmpty) { // Если есть хотя бы один кабель с нодами
-//            val systemCell = new Cell(1, 9).add(new Paragraph(system).setTextAlignment(TextAlignment.CENTER).setFont(gostFont).setBold())
-//            systemCell.setHorizontalAlignment(HorizontalAlignment.CENTER)
-//            table.addCell(systemCell)
-//
-//            // Добавляем строки с информацией о кабелях
-//            cablesWithNodes.foreach { cable =>
-//              val nodes = getNodes(cable.cable_id, filteredNodes, cablesRoutesList ) // Ноды конкретного кабеля
-//              val cable_specN = cable.cable_spec_short.replaceAll("""^.*? - """, "")
-//              table.addCell(new Cell().add(new Paragraph(cable.cable_id).setFont(gostFont)))
-//              table.addCell(new Cell().add(new Paragraph(cable_specN).setFont(gostFont))) // Марка кабеля
-//              table.addCell(new Cell().add(new Paragraph(cable.section).setFont(gostFont)))
-//              table.addCell(new Cell().add(new Paragraph(cable.total_length.toString.replace('.', ',')).setFont(gostFont)))  // длина
-//              table.addCell(new Cell().add(new Paragraph(cable.from_e_id).setFont(gostFont))) // Индекс откуда
-//              table.addCell(new Cell().add(new Paragraph(cable.from_zone_id).setFont(gostFont))) // Помещение откуда
-//              table.addCell(new Cell().add(new Paragraph(cable.to_zone_id).setFont(gostFont))) // Помещение куда
-//              table.addCell(new Cell().add(new Paragraph(cable.to_e_id).setFont(gostFont))) // Индекс куда
-//              table.addCell(new Cell().add(new Paragraph("").setFont(gostFont))) // Примечание
-//
-//              // Строка с нодами кабеля
-//              val nodesCell = new Cell(1, 9)
-//              nodesCell.add(new Paragraph(nodes).setFont(gostFontBold))
-//              table.addCell(nodesCell)
-//            }
-//          }
-//        }
-//
-//        //        printPageBorder(pdf, 1)  //delete
-//        //        printPageBorder(pdf, 2)
-//
-//        document.add(table)
-//        document.close()
-//
-//        println(file.toString)
-//        //        file.toString
-//        printDocBorder(file.toString)  //ссылка на второй файл с бордерами уже
-//      } catch {
-//        case e: Throwable =>
-//          println(e.toString)
-//          e.toString
-//      }
-//    } catch {
-//      case e: Throwable => println(e.toString)
-//        e.toString
-//    }
+  def createPdf(data: Seq[CablesPdf], filteredNodes: Seq[CableNodes], cablesRoutesList: Seq[CableRoutesList]): String = {
+    println("cableData in def")
+    try {
+//      val file = Files.createTempFile("spec", ".pdf")
+      //дата
+      val currentDate = LocalDate.now()
+      val year = currentDate.format(DateTimeFormatter.ofPattern("yyyy"))
+      val month = currentDate.format(DateTimeFormatter.ofPattern("MM"))
+      val day = currentDate.format(DateTimeFormatter.ofPattern("dd"))
 
-//  }
+      // создание директории
+      val dirPath = Paths.get("/files")
+      val yearPath = dirPath.resolve(year)
+      if (!Files.exists(yearPath)) {
+        Files.createDirectories(yearPath)
+      }
+
+      val monthPath = yearPath.resolve(month)
+      if (!Files.exists(monthPath)) {
+        Files.createDirectories(monthPath)
+      }
+
+      val dayPath = monthPath.resolve(day)
+      if (!Files.exists(dayPath)) {
+        Files.createDirectories(dayPath)
+      }
+
+      val randomDirName = UUID.randomUUID().toString.take(10)
+      val randomDirPath = dayPath.resolve(randomDirName)
+
+      if (!Files.exists(randomDirPath)) {
+        Files.createDirectories(randomDirPath)
+      }
+
+      // Создаем файл PDF в соответствующей директории
+      var fileName = "file.pdf"
+      var filePath = randomDirPath.resolve(fileName)
+      val file = Files.createFile(filePath)
+
+      println(file)
+      val gostFont = PdfFontFactory.createFont(FontProgramFactory.createFont("fonts/GOSTtypeA.ttf"), PdfEncodings.IDENTITY_H, EmbeddingStrategy.PREFER_NOT_EMBEDDED)
+      val gostFontBold = PdfFontFactory.createFont(FontProgramFactory.createFont("fonts/gost_2.304_Bold.ttf"), PdfEncodings.IDENTITY_H, EmbeddingStrategy.PREFER_NOT_EMBEDDED)
+      val writer = new PdfWriter(file.toString)
+      val pdf = new PdfDocument(writer)
+      val document = new Document(pdf,PageSize.A4.rotate())
+      val totalWidth = PageSize.A4.getHeight
+
+      printTitle(document, 1, totalWidth, gostFont, "КАБЕЛЬНЫЙ ЖУРНАЛ МАГИСТРАЛЬНЫХ КАБЕЛЕЙ", "200101-880-004ВК")  //добавляем таблицу с титульника
+      document.add(new AreaBreak())  //разрыв страницы
+
+//      создаем основную таблицу
+      val columnPercentages = Array(15F, 20F, 15F, 7F, 10F, 10F, 10F, 10F, 15F)
+      val pointColumnWidths = columnPercentages.map(p => (totalWidth * p / 100).toFloat)
+      val table = new Table(pointColumnWidths).useAllAvailableWidth()
+      table.setMarginBottom(60F)
+
+      //заполняем шапку основной таблицы
+      table.addHeaderCell(new Cell(2, 1).add(new Paragraph("Индекс кабеля").setFont(gostFont)))
+      table.addHeaderCell(new Cell(2, 1).add(new Paragraph("Марка кабеля").setFont(gostFont)))
+      table.addHeaderCell(new Cell(2, 1).add(new Paragraph("Число жил и сечение, мм2").setFont(gostFont)))
+      table.addHeaderCell(new Cell(2, 1).add(new Paragraph("Проектная длина, м").setFont(gostFont)))
+
+      val fromCell = new Cell(1, 2)
+      fromCell.add(new Paragraph("Откуда идет кабель").setFont(gostFont))
+      table.addHeaderCell(fromCell)
+      val toCell = new Cell(1, 2)
+      toCell.add(new Paragraph("Куда идет кабель").setFont(gostFont))
+      table.addHeaderCell(toCell)
+      // Примечание
+      table.addHeaderCell(new Cell().add(new Paragraph("Примечание").setFont(gostFont)))
+
+
+      val arr = Seq("индекс", "помещ", "помещ", "индекс", "")
+      arr.foreach(i => {
+        val call = new Cell()
+        call.add(new Paragraph(i).setFont(gostFont))
+        table.addHeaderCell(call)
+      })
+
+
+      pdf.addEventHandler(PdfDocumentEvent.END_PAGE, new FooterEventListener(pdf, document, totalWidth, gostFont))
+
+      //заполняем данными
+      try {
+        val groupedData = data.groupBy(_.system)
+        val sortedGroupedData = TreeMap(groupedData.toSeq.sortBy(_._1): _*)
+        sortedGroupedData.foreach { case (system, cables) =>
+          // Проверяем, есть ли хотя бы один кабель с нодами
+          val cablesWithNodes = cables.filter(cable => getNodes(cable.cable_id, filteredNodes, cablesRoutesList).nonEmpty)
+          //          println(sort(cablesWithNodes))
+          val sortedCablesWithNodes = sort(cablesWithNodes)
+
+
+          if (sortedCablesWithNodes.nonEmpty) { // Если есть хотя бы один кабель с нодами
+            val systemCell = new Cell(1, 9).add(new Paragraph(system).setTextAlignment(TextAlignment.CENTER).setFont(gostFont).setBold())
+            systemCell.setHorizontalAlignment(HorizontalAlignment.CENTER)
+            table.addCell(systemCell)
+
+            // Добавляем строки с информацией о кабелях
+            cablesWithNodes.foreach { cable =>
+              val nodes = getNodes(cable.cable_id, filteredNodes, cablesRoutesList ) // Ноды конкретного кабеля
+              val cable_specN = cable.cable_spec_short.replaceAll("""^.*? - """, "")
+              table.addCell(new Cell().add(new Paragraph(cable.cable_id).setFont(gostFont)))
+              table.addCell(new Cell().add(new Paragraph(cable_specN).setFont(gostFont))) // Марка кабеля
+              table.addCell(new Cell().add(new Paragraph(cable.section).setFont(gostFont)))
+              table.addCell(new Cell().add(new Paragraph(cable.total_length.toString.replace('.', ',')).setFont(gostFont)))  // длина
+              table.addCell(new Cell().add(new Paragraph(cable.from_e_id).setFont(gostFont))) // Индекс откуда
+              table.addCell(new Cell().add(new Paragraph(cable.from_zone_id).setFont(gostFont))) // Помещение откуда
+              table.addCell(new Cell().add(new Paragraph(cable.to_zone_id).setFont(gostFont))) // Помещение куда
+              table.addCell(new Cell().add(new Paragraph(cable.to_e_id).setFont(gostFont))) // Индекс куда
+              table.addCell(new Cell().add(new Paragraph("").setFont(gostFont))) // Примечание
+
+              // Строка с нодами кабеля
+              val nodesCell = new Cell(1, 9)
+              nodesCell.add(new Paragraph(nodes).setFont(gostFontBold))
+              table.addCell(nodesCell)
+            }
+          }
+        }
+
+        //        printPageBorder(pdf, 1)  //delete
+        //        printPageBorder(pdf, 2)
+
+        document.add(table)
+        document.close()
+
+        println(file.toString)
+        //        file.toString
+        printDocBorder(file.toString)  //ссылка на второй файл с бордерами уже
+      } catch {
+        case e: Throwable =>
+          println(e.toString)
+          e.toString
+      }
+    } catch {
+      case e: Throwable => println(e.toString)
+        e.toString
+    }
+
+  }
 
   def getNodes(cable_id: String, data: Seq[CableNodes], cablesRoutesList: Seq[CableRoutesList]): String = {  //выбираю ноды конкретного кабеля и формирую строку из них
     //    data.filter(_.cable_id == cable_id)
